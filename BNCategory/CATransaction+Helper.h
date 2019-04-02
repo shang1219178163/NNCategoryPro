6a4a216d254880a466d8f2d8e5c5387e8a3b3017
Shang <shang1219178163@gmail.com>
2019-04-02 15:49:15 +0800

    CATransaction document

--- BNCategory/CATransaction+Helper.h
+++ BNCategory/CATransaction+Helper.h
@@ -6,6 +6,12 @@
 //  Copyright © 2018年 Baymax. All rights reserved.
 //
 
+/**
+CATransaction是事务，用于批量提交多个对layer-tree的操作，并且是原子性的。所有对layer-tree的修改都必须包含在事务内。事务可以嵌套。
+ 
+ 所有对layer-tree的操作都必须处于事务。修改UIView的属性最终也是修改到了layer-tree。当我们改动到layer-tree时，如果当前没有显式创建过CATransaction，则系统会创建一个隐式的CATransaction，这个隐式CATransaction会在RunLoop结束后commit。
+ */
+
 #import <QuartzCore/QuartzCore.h>
 
 @interface CATransaction (Helper)
