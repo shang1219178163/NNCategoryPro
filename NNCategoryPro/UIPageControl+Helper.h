a5259c207641faeed56dfef08676e765dcc9e548
Shang <shang1219178163@gmail.com>
2021-01-07 12:21:52 +0800

    回调优化

--- NNCategoryPro/UIPageControl+Helper.h
+++ NNCategoryPro/UIPageControl+Helper.h
@@ -11,6 +11,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UIPageControl (Helper)
 
+- (void)addActionHandler:(void(^)(UIPageControl *sender))handler forControlEvents:(UIControlEvents)controlEvents;
+
 + (instancetype)createRect:(CGRect)rect numberOfPages:(NSInteger)numberOfPages;
 
 @end
--- NNCategoryPro/UIPageControl+Helper.m
+++ NNCategoryPro/UIPageControl+Helper.m
@@ -6,9 +6,22 @@
 //
 
 #import "UIPageControl+Helper.h"
+#import <objc/runtime.h>
 #import "UIColor+Helper.h"
 
 @implementation UIPageControl (Helper)
+
+- (void)addActionHandler:(void(^)(UIPageControl *sender))handler forControlEvents:(UIControlEvents)controlEvents{
+    [self addTarget:self action:@selector(p_handleActionPage:) forControlEvents:controlEvents];
+    objc_setAssociatedObject(self, _cmd, handler, OBJC_ASSOCIATION_COPY_NONATOMIC);
+}
+
+- (void)p_handleActionPage:(UIPageControl *)sender{
+    void(^block)(UIPageControl *control) = objc_getAssociatedObject(self, @selector(addActionHandler:forControlEvents:));
+    if (block) {
+        block(sender);
+    }
+}
 
 + (instancetype)createRect:(CGRect)rect numberOfPages:(NSInteger)numberOfPages{
     UIPageControl *pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(rect) - 30, CGRectGetWidth(rect), 30)];
